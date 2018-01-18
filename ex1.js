e092824e7eb122a8d3a874add48c427a6e3273af
LokeshDogga <34920115+LokeshDogga@users.noreply.github.com>
2018-01-18 14:55:20 +0530

    Update ex1.js

--- ex1.js
+++ ex1.js
@@ -1,79 +1,82 @@
-A();
+function temp(){
+		A();
 
-function C() {
-	console.log("C");
-	d();
-}
+	function C() {
+		console.log("C");
+		d();
+	}
 
-function E(f) {
-	console.log("E");
-	var f = F;
-	f();
+	function E(f) {
+		console.log("E");
+		var f = F;
+		f();
 
-}
+	}
 
-function A() {
-	console.log("A");
-	B();
-};
+	function A() {
+		console.log("A");
+		B();
+	};
 
-var C;
+	var C;
 
-function G() {
-	console.log("G");
-	H();
+	function G() {
+		console.log("G");
+		H();
 
-	function H() {
-		console.log("H");
-		I();
+		function H() {
+			console.log("H");
+			I();
+		};
+	}
+
+
+	function d() {
+		console.log("D");
+		E();
+	}
+
+	function I() {
+		console.log("I");
+		J();
+	}
+
+	function B() {
+		console.log("B");
+		C();
+	};
+
+	function F() {
+		console.log("F");
+		G();
+	};
+
+	function K(){
+		var rest = "KLMNOPQRSTUVWXYZ".split("");
+		for (var i=0; i<rest.length; i++) {
+			(function(i){
+				// define the current function
+				var temp = function() {
+					console.log(rest[i]);
+					if (i < (rest.length-1)) {
+						// TODO: call the next function
+					}
+				}();
+			})(i);
+		}
+	}
+
+	function J() {
+		var J = function() {
+			console.log("J");
+			K();
+		}();
+	};
+
+	C = function() {
+		console.log("C");
+		D();
 	};
 }
-
-
-function d() {
-	console.log("D");
-	E();
-}
-
-function I() {
-	console.log("I");
-	J();
-}
-
-function B() {
-	console.log("B");
-	C();
-};
-
-function F() {
-	console.log("F");
-	G();
-};
-
-function K(){
-	var rest = "KLMNOPQRSTUVWXYZ".split("");
-	for (var i=0; i<rest.length; i++) {
-		(function(i){
-			// define the current function
-			var temp = function() {
-				console.log(rest[i]);
-				if (i < (rest.length-1)) {
-					// TODO: call the next function
-				}
-			}();
-		})(i);
-	}
-}
-
-function J() {
-	var J = function() {
-		console.log("J");
-		K();
-	}();
-};
-
-C = function() {
-	console.log("C");
-	D();
-};
+temp();
 
